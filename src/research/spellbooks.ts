c06a1a6637c31d9b421d54b9d9825241a4ebc71d
sonnaya2 <299354192+sonnaya2@users.noreply.github.com>
2026-07-25 05:38:55 -0700

    Add typed spellbook access

---
+++ src/research/spellbooks.ts
@@ -0,0 +1,16 @@
+import spellbookSource from "../../data/reference/spellbooks.json";
+
+export type SpellbookRecord = (typeof spellbookSource)["spellbooks"][number];
+export type SpecialMagicSystem = (typeof spellbookSource)["special_magic_systems"][number];
+
+export function getSpellbooks(): SpellbookRecord[] {
+  return spellbookSource.spellbooks;
+}
+
+export function getSpellbook(id: SpellbookRecord["id"]): SpellbookRecord | undefined {
+  return spellbookSource.spellbooks.find((book) => book.id === id);
+}
+
+export function getSpecialMagicSystems(): SpecialMagicSystem[] {
+  return spellbookSource.special_magic_systems;
+}
